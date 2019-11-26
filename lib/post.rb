9d7007564619d9b1b979f3c487eb6848a3245f92
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-26 19:05:00 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,8 +2,8 @@ class Post
       attr_accessor :title, :author
       @@all = []
       
-    def initialize(name)
-      @name = name 
+    def initialize(title)
+      @title = title 
       @@all << self
     end
     
