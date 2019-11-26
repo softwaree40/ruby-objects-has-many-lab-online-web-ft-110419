f2e967ba58957e5ec01c84cbed39349144f217e4
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-26 18:53:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song 
       attr_accessor:artist,:name
       @@all = []
+      
       def initialize(name)
         @name = name
          @@all << self
       end
+      
       def self.all
         @@all
       end
