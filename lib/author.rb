af2e26fbd7f6d4852c70581bfa7319972bd1f128
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-26 19:03:40 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,4 +12,10 @@ class Author
     def add_post(post)
        post.author = self
      end
+      def add_post_by_title(post_name)
+          post = Post.new(post_name)
+          add_post(post)
+          #binding.pry
+          
+       end
 end
