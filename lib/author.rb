6820a42000a46d664e3f979204d2db05e2d6743f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-26 18:58:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,12 @@
 class Author 
       attr_accessor :name 
+      
    def initialize(name)
       @name = name
-     
    end
+   
      def posts
-       Post.all.select {|post| post.author}
+       Post.all.select {|post| post.author == self}
      end
   
 end
