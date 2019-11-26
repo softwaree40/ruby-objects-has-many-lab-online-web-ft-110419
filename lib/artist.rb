1707deccfeba29bf519d2d53e8124c9a92052c59
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-26 16:14:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,7 +19,7 @@ class Artist
      end
        def add_song_by_name(song_name)
           song = Artist.new(song_name)
-          self.songs << song
+          self.songs.name
           
           #binding.pry
       end
